a44cefdafbeeeb43fda109443c5281d4cc610e37
m.d <49080343+opp@users.noreply.github.com>
2024-11-30 12:49:25 -0500

    Update Refetch thumbnail button to be hidden away

--- chromium/main.js
+++ chromium/main.js
@@ -1,12 +1,21 @@
 "use strict";
 
 const API_ENDPOINT = "http://localhost:41415/api/";
+const existsIconPaths = {
+    48: "icons/exists/dalennod-exists-48.png",
+    96: "icons/exists/dalennod-exists-96.png",
+};
+const defaultIconPaths = {
+    48: "icons/dalennod-48.png",
+    96: "icons/dalennod-96.png",
+};
 
-const [overlayDiv, overlayText, checkmark, archiveWarn] = [
+const [overlayDiv, overlayText, checkmark, archiveWarn, moreOptions] = [
     document.querySelector("#done-overlay-div"),
     document.querySelector("#done-overlay-text"),
     document.querySelector("#checkmark"),
     document.querySelector("#archive-warn"),
+    document.getElementById("more-options"),
 ];
 const [btnCreate, btnUpdate, btnRemove, btnArchive, btnRefetchThumbnail] = [
     document.getElementById("button-add-req"),
@@ -65,7 +74,7 @@ const checkConnection = async () => {
         conn = true;
     }
     checkUrl(currTab.url);
-    await fillAllGroups(getGroups());
+    await fillAllGroups();
 };
 
 const checkUrl = async (currTabUrl) => {
@@ -74,14 +83,19 @@ const checkUrl = async (currTabUrl) => {
         method: "POST",
         body: JSON.stringify({ url: currTabUrl }),
     });
-    if (res.status != 404) {
+    if (res.status === 404) {
+        await chrome.action.setIcon({ path: defaultIconPaths });
+    } else {
         const receviedData = await res.json();
 
         btnUpdate.removeAttribute("hidden");
         btnRemove.removeAttribute("hidden");
         btnCreate.setAttribute("hidden", "");
         fillData(JSON.parse(JSON.stringify(receviedData)));
+
+        await chrome.action.setIcon({ path: existsIconPaths });
     }
+    return;
 };
 
 const fillData = (dataFromDb) => {
@@ -95,17 +109,16 @@ const fillData = (dataFromDb) => {
         ? btnArchive.setAttribute("hidden", "")
         : btnArchive.removeAttribute("hidden");
     dataFromDb.byteThumbURL
-        ? btnRefetchThumbnail.setAttribute("hidden", "")
-        : btnRefetchThumbnail.removeAttribute("hidden");
+        ? moreOptions.setAttribute("hidden", "")
+        : moreOptions.removeAttribute("hidden");
 };
 
-const getGroups = async () => {
+const fillAllGroups = async () => {
     const fetchUrl = API_ENDPOINT + "groups/";
     const res = await fetch(fetchUrl);
-    return await res.text();
+    const allGroups = await res.text();
+    bmGroupsList.innerHTML = allGroups;
 };
-const fillAllGroups = async (allGroups) =>
-    (bmGroupsList.innerHTML = await allGroups);
 
 btnCreate.addEventListener("click", () => addEntry());
 const addEntry = async () => {
@@ -131,18 +144,20 @@ const addEntry = async () => {
 
     if (res.ok) {
         resizeInput();
-    }
 
-    archiveWarn.setAttribute("hidden", "");
-    btnCreate.disabled = false;
-    checkmark.removeAttribute("hidden");
-    overlayText.innerHTML = "Created&nbsp;&check;";
-    overlayDiv.style.display = "block";
-    setTimeout(() => {
-        checkmark.setAttribute("hidden", "");
-        overlayDiv.style.display = "none";
-        window.close();
-    }, 2000);
+        archiveWarn.setAttribute("hidden", "");
+        btnCreate.disabled = false;
+        checkmark.removeAttribute("hidden");
+        overlayText.innerHTML = "Created&nbsp;&check;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            checkmark.setAttribute("hidden", "");
+            overlayDiv.style.display = "none";
+            window.close();
+        }, 1000);
+
+        checkUrl(dataJSON.url);
+    }
 };
 
 btnUpdate.addEventListener("click", () => updateEntry(bmId.innerHTML));
@@ -170,7 +185,9 @@ const updateEntry = async (idInDb) => {
             checkmark.setAttribute("hidden", "");
             overlayDiv.style.display = "none";
             window.close();
-        }, 2000);
+        }, 1000);
+
+        checkUrl(dataJSON.url);
     }
 };
 
@@ -179,16 +196,20 @@ const removeEntry = async (idInDb) => {
     const fetchURL = API_ENDPOINT + "delete/" + idInDb;
     const res = await fetch(fetchURL);
 
-    if (res.ok) resizeInput();
+    if (res.ok) {
+        resizeInput();
 
-    checkmark.removeAttribute("hidden");
-    overlayText.innerHTML = "Removed&nbsp;&check;";
-    overlayDiv.style.display = "block";
-    setTimeout(() => {
-        checkmark.setAttribute("hidden", "");
-        overlayDiv.style.display = "none";
-        window.close();
-    }, 2000);
+        checkmark.removeAttribute("hidden");
+        overlayText.innerHTML = "Removed&nbsp;&check;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            checkmark.setAttribute("hidden", "");
+            overlayDiv.style.display = "none";
+            window.close();
+        }, 1000);
+
+        checkUrl(currTab.url);
+    }
 };
 
 btnRefetchThumbnail.addEventListener("click", () =>
@@ -197,8 +218,21 @@ btnRefetchThumbnail.addEventListener("click", () =>
 const refetchThumbnail = async (idInDb) => {
     const fetchUrl = API_ENDPOINT + "refetch-thumbnail/" + idInDb;
     const res = await fetch(fetchUrl);
-    if (!res.ok) console.log("error refetching thumbnail. STATUS:", res.status);
-    // console.log(await res.text());
+    if (res.ok) {
+        overlayText.innerHTML = "Refetched thumbnail&nbsp;&check;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            overlayDiv.style.display = "none";
+            moreOptions.setAttribute("hidden", "");
+        }, 1000);
+    } else {
+        console.log("error refetching thumbnail. STATUS:", res.status);
+        overlayText.innerHTML = "Failed&nbsp;&cross;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            overlayDiv.style.display = "none";
+        }, 1000);
+    }
 };
 
 const resizeInput = () => {
--- firefox/main.js
+++ firefox/main.js
@@ -1,12 +1,21 @@
 "use strict";
 
 const API_ENDPOINT = "http://localhost:41415/api/";
+const existsIconPaths = {
+    48: "icons/exists/dalennod-exists-48.png",
+    96: "icons/exists/dalennod-exists-96.png",
+};
+const defaultIconPaths = {
+    48: "icons/dalennod-48.png",
+    96: "icons/dalennod-96.png",
+};
 
-const [overlayDiv, overlayText, checkmark, archiveWarn] = [
+const [overlayDiv, overlayText, checkmark, archiveWarn, moreOptions] = [
     document.querySelector("#done-overlay-div"),
     document.querySelector("#done-overlay-text"),
     document.querySelector("#checkmark"),
     document.querySelector("#archive-warn"),
+    document.getElementById("more-options"),
 ];
 const [btnCreate, btnUpdate, btnRemove, btnArchive, btnRefetchThumbnail] = [
     document.getElementById("button-add-req"),
@@ -74,14 +83,19 @@ const checkUrl = async (currTabUrl) => {
         method: "POST",
         body: JSON.stringify({ url: currTabUrl }),
     });
-    if (res.status != 404) {
+    if (res.status === 404) {
+        await browser.browserAction.setIcon({ path: defaultIconPaths });
+    } else {
         const receviedData = await res.json();
 
         btnUpdate.removeAttribute("hidden");
         btnRemove.removeAttribute("hidden");
         btnCreate.setAttribute("hidden", "");
         fillData(JSON.parse(JSON.stringify(receviedData)));
+
+        await browser.browserAction.setIcon({ path: existsIconPaths });
     }
+    return;
 };
 
 const fillData = (dataFromDb) => {
@@ -95,8 +109,8 @@ const fillData = (dataFromDb) => {
         ? btnArchive.setAttribute("hidden", "")
         : btnArchive.removeAttribute("hidden");
     dataFromDb.byteThumbURL
-        ? btnRefetchThumbnail.setAttribute("hidden", "")
-        : btnRefetchThumbnail.removeAttribute("hidden");
+        ? moreOptions.setAttribute("hidden", "")
+        : moreOptions.removeAttribute("hidden");
 };
 
 const fillAllGroups = async () => {
@@ -130,18 +144,20 @@ const addEntry = async () => {
 
     if (res.ok) {
         resizeInput();
-    }
 
-    archiveWarn.setAttribute("hidden", "");
-    btnCreate.disabled = false;
-    checkmark.removeAttribute("hidden");
-    overlayText.innerHTML = "Created&nbsp;&check;";
-    overlayDiv.style.display = "block";
-    setTimeout(() => {
-        checkmark.setAttribute("hidden", "");
-        overlayDiv.style.display = "none";
-        window.close();
-    }, 2000);
+        archiveWarn.setAttribute("hidden", "");
+        btnCreate.disabled = false;
+        checkmark.removeAttribute("hidden");
+        overlayText.innerHTML = "Created&nbsp;&check;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            checkmark.setAttribute("hidden", "");
+            overlayDiv.style.display = "none";
+            window.close();
+        }, 1000);
+
+        checkUrl(dataJSON.url);
+    }
 };
 
 btnUpdate.addEventListener("click", () => updateEntry(bmId.innerHTML));
@@ -169,7 +185,9 @@ const updateEntry = async (idInDb) => {
             checkmark.setAttribute("hidden", "");
             overlayDiv.style.display = "none";
             window.close();
-        }, 2000);
+        }, 1000);
+
+        checkUrl(dataJSON.url);
     }
 };
 
@@ -178,16 +196,20 @@ const removeEntry = async (idInDb) => {
     const fetchURL = API_ENDPOINT + "delete/" + idInDb;
     const res = await fetch(fetchURL);
 
-    if (res.ok) resizeInput();
+    if (res.ok) {
+        resizeInput();
 
-    checkmark.removeAttribute("hidden");
-    overlayText.innerHTML = "Removed&nbsp;&check;";
-    overlayDiv.style.display = "block";
-    setTimeout(() => {
-        checkmark.setAttribute("hidden", "");
-        overlayDiv.style.display = "none";
-        window.close();
-    }, 2000);
+        checkmark.removeAttribute("hidden");
+        overlayText.innerHTML = "Removed&nbsp;&check;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            checkmark.setAttribute("hidden", "");
+            overlayDiv.style.display = "none";
+            window.close();
+        }, 1000);
+
+        checkUrl(currTab.url);
+    }
 };
 
 btnRefetchThumbnail.addEventListener("click", () =>
@@ -196,8 +218,21 @@ btnRefetchThumbnail.addEventListener("click", () =>
 const refetchThumbnail = async (idInDb) => {
     const fetchUrl = API_ENDPOINT + "refetch-thumbnail/" + idInDb;
     const res = await fetch(fetchUrl);
-    if (!res.ok) console.log("error refetching thumbnail. STATUS:", res.status);
-    // console.log(await res.text());
+    if (res.ok) {
+        overlayText.innerHTML = "Refetched thumbnail&nbsp;&check;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            overlayDiv.style.display = "none";
+            moreOptions.setAttribute("hidden", "");
+        }, 1000);
+    } else {
+        console.log("error refetching thumbnail. STATUS:", res.status);
+        overlayText.innerHTML = "Failed&nbsp;&cross;";
+        overlayDiv.style.display = "block";
+        setTimeout(() => {
+            overlayDiv.style.display = "none";
+        }, 1000);
+    }
 };
 
 const resizeInput = () => {
